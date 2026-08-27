67c0a77f396999d0a5a8b105d2809d2cf8147245
Utkarsh Mishra <mdutkarsh11@gmail.com>
2026-08-27 23:32:11 +0530

    Add Power-of-Three (math)

---
+++ leetcode/math/326-Power-of-Three.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        while(n > 1 && n % 3 == 0){
+            n /= 3;
+        }
+        if(n == 1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
