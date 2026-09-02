2b1f429f17aacd7f2e0523a5d4394caad7ad985c
Utkarsh Mishra <mdutkarsh11@gmail.com>
2026-09-02 15:07:40 +0530

    Add Next Round (800)

---
+++ codeforces/800/158A - Next Round.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    
+    int n;
+    int k;
+    cin >> n >> k;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >>arr[i];
+    }
+
+    int count = 0;
+    for(int i = 0; i < n; i++){
+
+        if(arr[i] >= arr[k-1] && arr[i] > 0){
+            count++;
+        }
+        
+    }
+
+    cout<<count<<endl;
+
+    
+    return 0;
+}
