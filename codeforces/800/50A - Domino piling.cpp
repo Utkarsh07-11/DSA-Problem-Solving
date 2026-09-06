3d4bf40426c868c7fc1f16aa99a8d481678b1692
Utkarsh Mishra <mdutkarsh11@gmail.com>
2026-09-06 19:15:32 +0530

    Add Domino piling (800)

---
+++ codeforces/800/50A - Domino piling.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    
+    int x, y;
+    cin >> x;
+    cin >> y;
+    cout << (x * y) / 2 << endl;   
+    
+    return 0;
+}
